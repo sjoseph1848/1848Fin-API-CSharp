165530cb4c3654b9b1bcae16b417881f02687bb8
Sam <sjoseph1848@gmail.com>
2020-04-22 21:29:30 -0500

    updates to the INcome statement Model

---
+++ Fin/API.Data/API.Data/IncomeStatement.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace API.Data
+{
+    public class IncomeStatement
+    {
+
+    }
+}
---
+++ Fin/API.Data/API.Data/Models/IncomeStatementDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace API.Data.Models
+{
+    public class IncomeStatementDto
+    {
+        public string Symbol { get; set; }
+
+        public IncomeStatementDetailDto[] financials {get;set;}
+        
+    }
+}
