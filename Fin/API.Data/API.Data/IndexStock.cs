0bcde24e78d1a35292c44b0b4682fda7926f6bb5
sjoseph1848 <sjoseph1848@gmail.com>
2020-05-11 21:50:17 -0500

    added the index dto

---
+++ Fin/API.Data/API.Data/IndexStock.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace Fin.BL
+{
+    public class IndexStock
+    {
+        private static HttpClient _httpClient = new HttpClient();
+
+        public IndexStock()
+        {
+            _httpClient.BaseAddress = new Uri("https://financialmodelingprep.com/api/v3/historical-price-full/index/%5EGSPC");
+            _httpClient.Timeout = new TimeSpan(0, 0, 30);
+            _httpClient.DefaultRequestHeaders.Clear();
+        }
+    }
+}
---
+++ Fin/API.Data/API.Data/Models/IndexDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fin.BL.Models
+{
+    public class IndexDto
+    {
+        public string symbol { get; set; }
+
+        public IndexDetailDto[] Historical { get; set; }
+    }
+}
