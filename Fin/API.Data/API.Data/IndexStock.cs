60aef9d7aeeb21161215962c89ea3ea0c10c4169
sjoseph1848 <sjoseph1848@gmail.com>
2020-05-11 22:24:47 -0500

    fixed changes

--- Fin/API.Data/API.Data/IndexStock.cs
+++ Fin/API.Data/API.Data/IndexStock.cs
@@ -1,19 +1,32 @@
-﻿using System;
+﻿using Fin.BL.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Fin.BL
 {
     public class IndexStock
     {
-        private static HttpClient _httpClient = new HttpClient();
-
-        public IndexStock()
+        public async Task<IndexDto> GetIndexAsync()
         {
-            _httpClient.BaseAddress = new Uri("https://financialmodelingprep.com/api/v3/historical-price-full/index/%5EGSPC");
-            _httpClient.Timeout = new TimeSpan(0, 0, 30);
-            _httpClient.DefaultRequestHeaders.Clear();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://financialmodelingprep.com/api/v3/historical-price-full/index/");
+                client.Timeout = new TimeSpan(0, 0, 30);
+                client.DefaultRequestHeaders.Clear();
+                var response = await client.GetAsync("%5EGSPC");
+                string json;
+                //if(response.EnsureSuccessStatusCode())
+                using (var content = response.Content)
+                {
+                    json = await content.ReadAsStringAsync();
+                }               
+                return JsonConvert.DeserializeObject<IndexDto>(json);
+            }
         }
     }
 }
